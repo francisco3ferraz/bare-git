118f1337b38e5768d0c8882b39974ec9ce632df7
Francisco Ferraz <francisco3ferraz@gmail.com>
2025-08-16 00:13:50 +0100

    User basics

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/francisco3ferraz/bare-git
 
 go 1.24.6
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.41.0
+)
---
+++ internal/models/user.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        int       `json:"id" db:"id"`
+	Username  string    `json:"username" db:"username"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"-" db:"password_hash"`
+	IsAdmin   bool      `json:"is_admin" db:"is_admin"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+func (u *User) HashPassword(password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	if err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)); err != nil {
+		return false
+	}
+
+	return true
+}
